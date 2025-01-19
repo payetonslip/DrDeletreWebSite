30730067969cd8a271793db664233d27680d0d84
RoxaneKirchmeyer <roxane.kirchmeyer@gmail.com>
2025-01-19 14:53:17 +0100

    add txt cabinet

--- views/medical-office.php
+++ views/medical-office.php
@@ -1,38 +1,40 @@
 <h1>Le cabinet</h1>
 <article class="presa-office">
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius alias in nemo perferendis? Amet dolor enim voluptate doloremque, nihil incidunt doloribus aliquid. Accusantium, earum! Tempora architecto eligendi debitis dignissimos laudantium.
-        Accusantium voluptatibus voluptates est corporis ad facilis quis nobis ex aut harum asperiores minima molestias dicta deserunt quas placeat recusandae ea quasi similique, deleniti debitis iste excepturi. Ratione, sit dolorem.
-        Quas minus necessitatibus, minima, alias corporis unde fugit veritatis porro obcaecati, aliquid reprehenderit! Eum at, reiciendis perferendis ea sapiente soluta animi repudiandae saepe quod sint fugit ducimus, id explicabo optio.
+        Médecin généraliste depuis plusieurs années, le Docteur DELETRE Nicolas vous accueille à son cabinet pour des consultations d’alopécie (chute des cheveux), de médecine régénérative et de médecine esthétique, morphologique et anti-âge.
+
+        La prise de rendez-vous s’effectue par Doctolib, et tout acte nécessite une consultation initiale avant programmation de soins à visée esthétique.
+
+        Il vous reçoit :
+
+        5 avenue de Lapeyrière, 31240 Saint Jean.
+
+        En téléconsultation dédiée à ces actes si nécessaire.
     </p>
-    <img src="../img/medical-office.jpg" alt="">
+    <img src="../img/medical-office2.jpg" alt="">
 </article>
-<h2>Ses formations</h2>
+<h2>Son parcours</h2>
 <article class="formation">
-<img src="../img/img-1.jpg" alt="">
-    
+    <img src="../img/dr-nicolas-deletre.jpg" alt="">
+
     <div>
         <section>
-            <h3>ESTHETHIQUE</h3>
+            <h3>ESTHETIQUE:</h3>
             <ul>
-                <li>– Diplôme inter-universitaire de Médecine Morphologique et Anti-âge</li>
-                <li>– Diplôme universitaire des Injections à visée esthétique</li>
-                <li>– Diplôme universitaire des Lasers à visée esthétique</li>
-                <li>– Diplôme universitaire de Greffes capillaires</li>
+                <li>- Formation initiale du Collège International de Médecine Esthétique.
+                    <br><br>2019-2020
+                    <br><br>2020-2021
+                </li>
+                <li>- Formation approfondie en implantologie faciale à visée esthétique, C.I.M.E
+                <br><br>2020-2021</li>
             </ul>
         </section>
         <section>
-            <h3>NUTRITION</h3>
-            <p>– Diplôme universitaire international francophone de diététique, nutrition clinique et thérapeutique</p>
-        </section>
-        <section>
-            <h3>MEDECINE DU SPORT</h3>
+            <h3>FORMATION CONTINUE:</h3>
             <ul>
-                <li>– Capacité de biologie et de médecine du Sport</li>
-                <li>– Diplôme inter-universitaire de Sport-Santé</li>
-                <li>– Diplôme inter-universitaire de Médecine Subaquatique et Hyperbare (médecine de plongée)
-                    MESOTHERAPIE A VISEE ANTALGIQUE (médecine du sport, rhumatologie)</li>
-                <li>– Diplôme inter-universitaire de Mésothérapie à visée antalgique</li>
+            <li>- Participation annuelle au congrès DEFEE (Dermatologues Fédérés Experts en Esthétique)</li>
+            <li>- Participation annuelle au congrès France Médecine Esthétique</li>
+            <li>- Participation au congrès 2024 AMWC Monaco (Aesthetic & Anti-Aging Medicine World Congress)</li>
             </ul>
         </section>
     </div>
